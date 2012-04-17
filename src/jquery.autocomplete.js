e672dc0ca3f634bc9af323cbe301efb3afc89d53
Vyacheslav Bazhinov <SlNPacifist@gmail.com>
2012-04-17 19:31:49 +0700

    Added "showEmptyResults" option. Does not make sense without "decorateResults" option, but still can be used to show something like "no results matched" via css content.

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -65,6 +65,7 @@
         selectFirst: false,
         selectOnly: false,
         showResult: null,
+        showEmptyResults: false,
         preventDefaultReturn: true,
         preventDefaultTab: false,
         autoFill: false,
@@ -795,7 +796,7 @@
         var $ul = $('<ul></ul>');
         var i, result, $li, autoWidth, first = false, $first = false;
 
-        if (numResults) {
+        if (numResults || this.options.showEmptyResults) {
             for (i = 0; i < numResults; i++) {
                 result = results[i];
                 $li = this.createItemFromResult(result);
