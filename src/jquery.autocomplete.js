ca698c14196ce1211cd98ba1317628bb185714ed
Vyacheslav Bazhinov <SlNPacifist@gmail.com>
2012-04-24 18:25:35 +0700

    Added option 'getData', which is a function returning set of data (as opposed to static 'data' option)

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -80,7 +80,8 @@
         useDelimiter: false,
         delimiterChar: ',',
         delimiterKeyCode: 188,
-        processData: null
+        processData: null,
+        getData: null
     };
 
     /**
@@ -536,6 +537,8 @@
             processResults([], value);
         } else if (this.options.data) {
             processResults(this.options.data, value);
+        } else if (this.options.getData) {
+            processResults(this.options.getData(), value);
         } else {
             this.fetchRemoteData(value, function(remoteData) {
                 processResults(remoteData, value);
